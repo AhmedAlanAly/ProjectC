a471e87a0fb622399ed8f11d282145463cc3ac16
araslan <araslan@localhost>
2024-01-03 20:03:13 -0600

    Initial Commit

---
+++ src/c/DemoC.java
@@ -0,0 +1,15 @@
+package c;
+
+public class DemoC {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Wednsday");
+		
+		System.out.println("Batch11");
+		System.out.println("Batch 11 Git Lecture");
+
+	}
+
+
+}
